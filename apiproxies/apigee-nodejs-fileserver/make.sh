744f1ee0d926258aea97aea5f1021475d87d7d3b
Alex Koo <akoo@apigee.com>
2015-11-15 14:00:04 -0800

    done

--- apiproxies/apigee-nodejs-fileserver/make.sh
+++ apiproxies/apigee-nodejs-fileserver/make.sh
@@ -4,4 +4,5 @@ cd apiproxies/apigee-nodejs-fileserver
 npm install
 cd node
 npm install
+cd ..
 grunt --env=test --curl=true --username=akoo@apigee.com --password=$ae_password --upload-modules=true --debug=true
