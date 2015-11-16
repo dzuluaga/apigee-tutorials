07a1a0138324fd40523f3ab1a1b5d01250e143b1
Alex Koo <akoo@apigee.com>
2015-11-16 13:25:15 -0800

    better sample

--- apiproxies/apigee-nock-mock/make.sh
+++ apiproxies/apigee-nock-mock/make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apigeetool deploynodeapp -u $ae_username -o $org -e $env -n 'diego-nock-mock' -d . -m app.js -b /diego-nock-mock
+apigeetool deploynodeapp -u $ae_username -p $ae_password -o $org -e $env -n 'diego-nock-mock' -d . -m app.js -b /diego-nock-mock
